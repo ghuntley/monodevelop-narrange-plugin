58ff315d659c07bfb1181533167ccaa3434d3450
Geoffrey Huntley <ghuntley@ghuntley.com>
2013-10-13 00:00:14 +1100

    added some notes from irc, autoload not working

--- src/MonoDevelop.NArrange/NArrangeHandler.cs
+++ src/MonoDevelop.NArrange/NArrangeHandler.cs
@@ -18,9 +18,6 @@ namespace MonoDevelop.NArrange
 
             try
             {
-                IdeApp.Workbench.AutoReloadDocuments = true;
-                IdeApp.Workbench.LockGui ();
-
                 var solutionDirectory = IdeApp.ProjectOperations.CurrentSelectedSolution.BaseDirectory;
 
                 var command = "/Users/ghuntley/.dotfiles/bin/narrange";
@@ -36,18 +33,31 @@ namespace MonoDevelop.NArrange
             }
             finally
             {
-                IdeApp.Workbench.AutoReloadDocuments = false;
 
-// HACK: Attempt at getting open files to reload automatically
-// as AutoReloadDocuments isn't detecting that the files
-// have been modified.
-//                var openfiles = IdeApp.Workbench.Documents;
-//                foreach (var document in openfiles)
-//                {
-//                    document.Reload();
-//                }
+                // HACK: Still not working..
+                //                var openfiles = IdeApp.Workspace.Items;
+                //
+                //                foreach (var file in openfiles)
+                //                {
+                //                    FileService.NotifyFileChanged(file.FileName);
+                //                }
 
-                IdeApp.Workbench.UnlockGui ();
+                //                [23:13:07] <Therzok>     ghuntley: check monodevelop's codeissuepad
+                //                [23:13:28] <Therzok>     it's used for batch fixing of source analysis issues.
+                //                [23:14:14] <ghuntley>    the git plugin uses IdeApp.Workbench.AutoReloadDocuments = true and locks the gui as part of a branch switch.
+                //                [23:14:50] <ghuntley>    lifted the pattern from there. Still quite new to monodevelop addons.
+                //                [23:19:28] <ghuntley>    @Therzok: If I run the https://github.com/ghuntley/monodevelop-narrange-plugin/blob/master/src/MonoDevelop.NArrange/NArrangeHandler.cs#L26 to #L27 in a consle/terminal then it is a different story. File changes are detected and monodevelop notifies me that the files have changed and asks to reload them.
+                //                [23:29:33] <Therzok>     Also, I don't think that you should lock/unlock the gui, but use DispatchService.
+                //                [23:33:17] <Therzok>     ghuntley: Also, on each file you modify, you need to call FileService.NotifyFilesChanged (myFiles);
+
+                // HACK: Attempt at getting open files to reload automatically
+                // as AutoReloadDocuments isn't detecting that the files
+                // have been modified.
+                //                var openfiles = IdeApp.Workbench.Documents;
+                //                foreach (var document in openfiles)
+                //                {
+                //                    document.Reload();
+                //                }
             }
         }
 
